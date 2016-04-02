c1d8776a6fc900f57cd8c151e0ddba7ceb461f85
Trevor Davis <threesevenths@gmail.com>
2016-04-02 12:35:53 -0700

    Added 6 more API keys

--- src/ReactionRoullete/Services/EmotionServiceApiKeyProvider.cs
+++ src/ReactionRoullete/Services/EmotionServiceApiKeyProvider.cs
@@ -16,6 +16,12 @@ namespace ReactionRoullete.Services
             _ApiKeys.Add("9799c513119d49c0a905af880b501906");
             _ApiKeys.Add("b0c2c88edec84f4b885eb277bfa5d613");
             _ApiKeys.Add("445c2dcbe89d4c69b1f723bd47acb2c0");
+            _ApiKeys.Add("0b174bbafc15476fb4554555fc4e1bad");
+            _ApiKeys.Add("c29c094910ae4af29e058e877a5b66b4");
+            _ApiKeys.Add("6d752e51341e4e1db1f672d2d62299a9");
+            _ApiKeys.Add("69b86fc94e1e47de8ea4b415e3947809");
+            _ApiKeys.Add("edcb849a586c48509e006cfe10b0101c");
+            _ApiKeys.Add("2905e7d0855f4400af5e307de0dce13b");
         }
 
         public EmotionServiceApiKeyProvider()
