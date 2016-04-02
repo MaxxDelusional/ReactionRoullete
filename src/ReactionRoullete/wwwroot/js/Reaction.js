5d8caa1b6c5390e48a355b0c7a8dfbc1ff6d3988
Trevor Davis <Trevor Davis>
2016-04-01 22:38:16 -0700

    Work on the front end RTC connection

---
+++ src/ReactionRoullete/wwwroot/js/Reaction.js
@@ -0,0 +1,31 @@
+﻿//Inspiration: https://github.com/muaz-khan/RecordRTC/blob/master/RecordRTC-to-ASPNETMVC/RecordRTC_to_ASPNETMVC/Views/RecordRTC/Index.cshtml
+
+$(function () {
+
+    var reactbutton = $("#React");
+    var stopbutton = $("#Stop");
+    
+    var recorder;
+
+    stopbutton.attr('disabled', true);
+
+    reactbutton.click(function () {
+        reactbutton.attr('disabled', true);
+        
+
+        navigator.getUserMedia({ audio: false, video: true }, function (stream) {
+            recorder = RecordRTC(stream, { type: "video" });
+            recorder.startRecording();
+
+            stopbutton.attr('disabled', false);
+        }, function (error) {
+            alert(JSON.stringify(error, null, '    '));
+            stopbutton.attr('disabled', false);
+        });
+    });
+
+    stopbutton.click(function () {
+        reactbutton.attr('disabled', false);
+        stopbutton.attr('disabled', true);
+    })
+});
